4468e6191be3b5df4eb9422bf071fd6d70724794
pjpure <pure254326@gmail.com>
2022-04-13 21:21:45 +0700

    create folders and files

---
+++ src/components/CardPlayer/CardPlayer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CardPlayer() {
+  return (
+    <div>CardPlayer</div>
+  )
+}
+
+export default CardPlayer
---
+++ src/components/CardTeam/CardTeam.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CardTeam() {
+  return (
+    <div>CardTeam</div>
+  )
+}
+
+export default CardTeam
