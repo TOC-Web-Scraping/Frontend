f59ad2d531f73798ff933a10ac38a4631924a644
pjpure <pure254326@gmail.com>
2022-04-14 00:51:57 +0700

    add navbar and router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./layout/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import Teams from "./pages/Teams/Teams";
+import Players from "./pages/Players/Players";
+import Team from "./pages/Team/Team";
+import Player from "./pages/Player/Player";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/players" element={<Players />} />
+          <Route path="/team/:id" element={<Team />} />
+          <Route path="/player/:id" element={<Player />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
