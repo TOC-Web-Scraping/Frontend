4fe0180bb09b2cab7b30323da25330ed41d02f2b
pjpure <pure254326@gmail.com>
2022-04-29 04:17:17 +0700

    clean warning

--- src/pages/Maps/Maps.jsx
+++ src/pages/Maps/Maps.jsx
@@ -1,35 +1,21 @@
 import React from "react";
 import CardSlider from "../../components/CardSlider/CardSlider";
-import { useNavigate } from "react-router-dom";
 import { useMaps } from "../../hooks/useMaps.js";
 import Loader from "../../components/Loader/Loader";
-import BackButton from "../../components/BackButton/BackButton";
-import './Map.css'
+import "./Map.css";
 
 function Maps() {
-  const navigate = useNavigate();
   const { data: maps, isLoading, error, isError } = useMaps();
 
   if (isError)
     return <h1 style={{ color: "white" }}>Error : {error.message}</h1>;
 
-  const onCardClick = (id) => {
-    if (id) {
-      navigate(`/maps/${id}`);
-    }
-  };
-
   return (
-    
     <>
       <div id="title-map">MAP</div>
       <div id="body">
         {isLoading && <Loader />}
-<CardSlider maps={maps} />
-                  
-                
-        
-        
+        <CardSlider maps={maps} />
       </div>
     </>
   );
